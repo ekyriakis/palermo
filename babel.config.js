f4ba686b40d1165f59105108c309e3d2a55ac6b4
Eleftherios Kyriakis <kiriakis.lefteris@gmail.com>
2025-12-29 18:56:36 +0200

    Initial commit109412414324234234234

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxRuntime: 'automatic' }],
-      ['@babel/preset-react', { runtime: 'automatic' }],
-    ],
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
   };
 };
